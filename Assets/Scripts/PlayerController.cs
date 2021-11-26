5c27f54ff02650d7324210cfbeb3fb424d989ab8
AmiRadoM <amirrado10@gmail.com>
2021-11-26 16:53:36 +0200

    Added smoothing to the focus

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,42 @@ public class PlayerController : MonoBehaviour
     public MouseLook camera;
     public LayerMask InteractableLayers;
 
+	bool move;
+	Vector3 newPos;
+
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.E))
+
+		if (Input.GetKeyDown(KeyCode.E) )
 		{
 			RaycastHit hit;
-			if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 100, InteractableLayers))
+			if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 100, InteractableLayers))
 			{
-				GameObject oven = hit.transform.GetComponentInParent<Interactable>().gameObject;
-				transform.position = oven.transform.position + hit.transform.GetComponentInParent<Interactable>().playerPositionOffset;
-				transform.eulerAngles = hit.transform.GetComponentInParent<Interactable>().playerRotation;
+				
 				camera.xRotationClamp = hit.transform.GetComponentInParent<Interactable>().xRotationClamp;
 				camera.yRotationClamp = hit.transform.GetComponentInParent<Interactable>().yRotationClamp;
+				transform.eulerAngles = hit.transform.GetComponentInParent<Interactable>().playerRotation;
+				move = true;
+				newPos = hit.transform.GetComponentInParent<Interactable>().transform.position + hit.transform.GetComponentInParent<Interactable>().playerPositionOffset;
 			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			camera.xRotationClamp = new Vector2(-90,90);
+			camera.yRotationClamp = new Vector2(0,360);
+			move = true;
+			newPos = new Vector3(-2,0,4);
+		}
+
+		if (move)
+		{
+			transform.position = Vector3.Lerp(transform.position , newPos, 2 * Time.deltaTime);
+			if(transform.position == newPos)
+			{
+				move = false;
+			}
+			
 		}
 	}
 }
