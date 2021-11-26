47c96d5766e15db82931a3d44336d37005941ee2
AmiRadoM <amirrado10@gmail.com>
2021-11-26 16:21:57 +0200

    Finished With The Focusing of the oven

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,16 @@ public class PlayerController : MonoBehaviour
 
 	private void Update()
 	{
-		if (Input.GetKey(KeyCode.E))
+		if (Input.GetKeyDown(KeyCode.E))
 		{
 			RaycastHit hit;
-			print("A");
 			if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 100, InteractableLayers))
 			{
 				GameObject oven = hit.transform.GetComponentInParent<Interactable>().gameObject;
 				transform.position = oven.transform.position + hit.transform.GetComponentInParent<Interactable>().playerPositionOffset;
 				transform.eulerAngles = hit.transform.GetComponentInParent<Interactable>().playerRotation;
+				camera.xRotationClamp = hit.transform.GetComponentInParent<Interactable>().xRotationClamp;
+				camera.yRotationClamp = hit.transform.GetComponentInParent<Interactable>().yRotationClamp;
 			}
 		}
 	}
